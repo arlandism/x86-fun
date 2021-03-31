dc2f61126a25848a4889f77d5b554fe0a2e8d42c
Arlandis Word <arlandis@remind101.com>
2021-03-31 10:18:06 -0400

    Terser binary converter
    key insights:
    - the loop is easier to setup if I inc rdi and go to memory every time.
      not sure if this reduces performance but won't worry about it for now.
    - checking the ascii bit patterns can be simplied to just checking if
      the rightmost bit is set.

--- binary_convert.asm
+++ binary_convert.asm
@@ -2,27 +2,14 @@ section .text
 global binary_convert
 binary_convert:
   xor rax, rax
-  mov r9, [rdi]
 _loop_start:
-  mov r8, r9
-  shr r9, 8
-  and	r8, 0xff ;; extract and clear rightmost byte, store in r8
+  movzx r9, byte [rdi]
+  cmp r9, 0
   jz _end ;; zero bit set by the and, end if null byte
-
-  ;; make room for next bit
-  shl rax, 1
-
-  ;; did we match 1 or 0; either way, add that to the pattern
-  mov r11, 1
-  cmp r8, 0x31
-  cmovz r10, r11
-
-  mov r11, 0
-  cmp r8, 0x30
-  cmovz r10, r11
-
-  or rax, r10
-
+  shl rax, 1 ;; make room for next bit
+  and r9, 1 ;; check the rightmost bit - 0x31 ('1') will have a 1 set
+  or rax, r9 ;; add bit to return result
+  inc rdi
   jmp _loop_start
 _end:
 	ret
