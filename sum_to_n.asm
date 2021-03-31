4380a5412176e5a2a67d0843cb1dd396529214d2
Arlandis Word <arlandis@remind101.com>
2021-03-30 21:32:15 -0400

    much simpler sum

--- sum_to_n.asm
+++ sum_to_n.asm
@@ -2,12 +2,8 @@ section .text
 global sum_to_n
 sum_to_n:
   xor rax, rax
-  mov r8, rdi
 _loop:
-  cmp r8, $0
-  jz _end
-  add rax, r8
-  dec r8
-  jmp _loop
-_end:
+  add rax, rdi
+  sub rdi, 1
+  jg _loop
 	ret
