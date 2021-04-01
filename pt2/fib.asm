cb797855734192696ee8164f8d8a106760820f46
Arlandis Word <arlandis@remind101.com>
2021-04-01 13:38:46 -0400

    add pt2

---
+++ pt2/fib.asm
@@ -0,0 +1,19 @@
+section .text
+global fib
+fib:
+	push rdi ;; save so that we can use the same register for both recursive calls w/o mutation
+	mov rax, rdi
+	cmp rax, 1
+	jle _end
+
+	dec rdi
+	call fib
+	push rax
+
+	dec rdi
+	call fib
+	pop r8
+	add rax, r8
+_end:
+	pop rdi ;; so caller can reuse (i.e. for the 2nd recursive call)
+	ret
