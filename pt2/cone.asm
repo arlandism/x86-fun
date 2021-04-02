835df28ac0352457f751c2ef39dbe2524f1b1449
Arlandis Word <arlandis@remind101.com>
2021-04-02 10:52:54 -0400

    add cone

--- pt2/cone.asm
+++ pt2/cone.asm
@@ -3,4 +3,13 @@ default rel
 section .text
 global volume
 volume:
- 	ret
+    mulss xmm0, xmm0
+    mulss xmm0, xmm1
+    mulss xmm0, [PI]
+    divss xmm0, [THREE]
+    ret
+
+section .rodata
+
+PI: dd 3.14159265359
+THREE: dd 3.0
